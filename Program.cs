e8e4330b7b350dac246406d1d653a10d87514bcf
Poornika R <poornika0406@gmail.com>
2026-09-23 19:05:08 +0530

    DAY 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("First step towards my .NET Journey");
+﻿Console.WriteLine("===== STUDENT DETAILS =====");
+
+Console.WriteLine("Name: Poornika");
+Console.WriteLine("Course: Computer Science");
+Console.WriteLine("Semester: 5");
+Console.WriteLine("Language: C#");
+Console.WriteLine("Goal: .NET Developer");
+
+Console.WriteLine("===========================");
