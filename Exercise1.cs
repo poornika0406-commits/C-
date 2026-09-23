ea9c15b283ff7f844ab27a5bdb29c71d780a6130
Poornika R <poornika0406@gmail.com>
2026-09-23 19:23:29 +0530

    DAY 1

---
+++ Exercise1.cs
@@ -0,0 +1,15 @@
+class Exercise1
+{
+    public static void Run()
+    {
+        Console.WriteLine("==========");
+        Console.WriteLine("RESUME");
+        Console.WriteLine("===========");
+        Console.WriteLine("NAME: ");
+        Console.WriteLine("DOB");
+        Console.WriteLine("QUALIFICATION");
+        Console.WriteLine("SKILLS");
+        Console.WriteLine("CERTIFICATION");
+        Console.WriteLine("CONTACT");
+    }
+}
